cea61fa725c0ed35c648858162e3f47b838ca93a
Spoorthish <98159457+Spoorthish@users.noreply.github.com>
2022-03-16 12:00:12 +0530

    Add files via upload

---
+++ employeeform/registration.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * I95dev_employeeform Module Registration
+ *
+ * @category    I95dev
+ * @package     I95dev_employeeform
+ * @author      I95dev Software Private Limited
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'I95dev_employeeform',
+    __DIR__
+);
+?>
